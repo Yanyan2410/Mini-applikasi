67a83433b8d064e4be8689f3ff636db9ec3ed080
Yanyan2410 <yanyangatot1059@gmail.com>
2020-06-03 12:22:19 +0700

    MiniApp Team Assignment2

---
+++ TeamAssignment2.py
@@ -0,0 +1,80 @@
+# ################### Team Assignment
+# ##### Kelompok 2-3 orang
+
+# Ketika di run awal :
+# MENU -
+# Login dan Register
+
+# - Register 
+# Masukkan data 
+# USER ID : ..... (harus kombinasi huruf dan angka) ==== lakukan pengecekan duplikasi Tidak boleh ada User ID yang sama
+# Password : ... (harus gabungan huruf kapital, huruf kecil dan angka, minimal 8 karakter)
+# Email : ..... (lakukan verifikasi email)
+# Ketentuan valid dan tidak valid sesuai tugas sebelumnya
+
+# Nama : ...
+# Gender : .... 
+# usia : ..... 
+# pekerjaan : .....
+# hobi : (isi lebih dari satu)
+# alamat :
+# Nama kota :
+# RT :
+# RW :
+# Zip Code : 
+# Geo : Lat - Langitude
+# No HP : ..... 
+
+# Simpan (Y/N) 
+# -- kalau Y : Data tersimpan
+# -- kalau N : data tidak tersimpan
+
+# Baik Y atau N  == Kembali ke Menu awal
+
+# Login 
+# Register 
+
+# - Login
+# Masukkan ID dan Password == Lakukan pengecekan ID dan Password
+# ## ID salah atau Password salah atau ID Tidak terdaftar
+# - Anda Berhasil Login 
+
+# ### MENU UTAMA
+
+# 1. Data Personal
+# di klik : keluar data personal 
+# Data Anda adalah :
+# Nama : ...
+# Email : .....
+# Gender : .... 
+# usia : ..... 
+# pekerjaan : .....
+# hobi : (isi lebih dari satu)
+# alamat :
+# Nama kota :
+# RT :
+# RW :
+# Zip Code : 
+# Geo : Lat - Langitude
+# No HP : ..... 
+
+# 2. Konversi Kode Morse
+# 3. Kalkulator : (atau bisa kalian ganti dengan project lain)
+# 4. Konversi Romawi
+# 5. Hitung hari # masukkan hari terus angka, 100 hari dari hari ini adalah ....
+# 6. Kamus Hari (ENG - IND & IND - ENG) # Auto Detect
+# 7. Manipulasi karakter : (atau bisa kalian ganti dengan project lain)
+# 8. Konversi jumlah hari (dari inputan ubah ke tahun, bulan, minggu, hari)
+# 9. Nilai Faktorial 
+# 10. Jumlah angka Fibonacci (sesuai inputan user) misal user input 7 (jumlah 7 deret pertama bilangan fibonacci dan keluarkan deret nya)
+# 11. Data USER === menampilkan user ID dan Password serta email dari seluruh data yg ada
+# 12. MENU CRUD:
+# opsi:
+# Nama barang dan Stok = Gudang
+# Nama barang dan harga = Toko-kasir
+# Nama karyawan dan gaji = Penggajian
+# Nama Mahasiswa dan nilai = Kampus
+
+# ** Optional : Boleh tambahkan fungsi lain
+# *** Coba gunakan Def, Lambda, filter, reduce dan map
+# **** Kirim Email : Deadline nya 31 Mei 2020
